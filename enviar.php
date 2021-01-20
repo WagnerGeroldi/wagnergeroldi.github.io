8b03ad70dfd6cda0a8e95298cff941be6098e74e
WagnerGeroldi <74829196+WagnerGeroldi@users.noreply.github.com>
2021-01-20 01:26:13 -0300

    novidades

---
+++ enviar.php
@@ -0,0 +1,21 @@
+﻿<?php
+    $para = "wagnergeroldi@gmail.com";
+    $name = $_POST['name'];
+    $city = $_POST['city'];
+    $date = $_POST['date'];
+	$message = $_POST['message'];
+    $mensagem = "name: $name<br>";
+    $mensagem .= "city: $city<br>";
+    $mensagem .= "date: $date<br>";
+	$mensagem .= "message: $message<br>";
+    $headers = 'From: '.$email."\r\n". 'Reply-To: '.$email."\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+    if (mail($para, $name, $menssagem, $headers)){
+        echo "Sua mensagem foi enviada com sucesso!";
+    }
+    else{
+        echo "Aconteceu um erro, tente novamente mais tarde.";
+    }
+?>
